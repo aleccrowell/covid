fixing growth rate calculation

covid/models/base.py
@@ -12,7 +12,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 import numpy as onp
-from covid.compartment import SEIRDModel
+from covid.compartment import SEIHRModel
 
 
 '''Utility to define access method for time varying fields'''
@@ -39,11 +39,11 @@ class Model():
         'D': 'dead',
         'C': 'cumulative infected',
         'y': 'confirmed',
-        'z': 'deaths',
+        'z': 'hosps',
         'dy': 'daily confirmed',
-        'dz': 'daily deaths',
+        'dz': 'daily hosps',
         'mean_dy': 'daily confirmed (mean)',
-        'mean_dz': 'daily deaths (mean)'
+        'mean_dz': 'daily hosps (mean)'
     }
             
     
@@ -139,7 +139,7 @@ class Model():
         sigma = self.mcmc_samples['sigma']
         beta_end = beta[:,-rw_use_last:].mean(axis=1)
 
-        growth_rate = SEIRDModel.growth_rate((beta_end, sigma, gamma))
+        growth_rate = SEIHRModel.growth_rate((beta, gamma))
         
         low = int(low/100 * len(growth_rate))
         high = int(high/100 * len(growth_rate))

covid/util.py
@@ -156,7 +156,7 @@ def plot_growth_rate(mcmc_samples, start, model=SEIRModel, ax=None):
     gamma = mcmc_samples['gamma'][:,None]
     t = pd.date_range(start=start, periods=beta.shape[1], freq='D')
 
-    growth_rate = SEIRModel.growth_rate((beta, sigma, gamma))
+    growth_rate = model.growth_rate((beta, gamma))
 
     pi = rnp.percentile(growth_rate, (10, 90), axis=0)
     df = pd.DataFrame(index=t, data={'growth_rate': rnp.median(growth_rate, axis=0)})
@@ -409,7 +409,8 @@ def score_place(forecast_date,
     log_score = prob.apply(np.log).clip(lower=-10).rename('log score')
     scores['log_score'] = log_score
 
-    # Compute quantile of observed value in samples
+    # Compute quantile of 
+    # d value in samples
     scores['quantile'] = samples.lt(obs, axis=0).sum(axis=1) / n_samples
     
     return scores